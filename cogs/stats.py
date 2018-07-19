ecbeb8a03f7d580038ede5c0fb41255591e5ee80
James <37597405+zabtc@users.noreply.github.com>
2018-07-19 04:13:47 +0200

    setup stats API for NORT

--- cogs/stats.py
+++ cogs/stats.py
@@ -12,7 +12,7 @@ class Stats:
     @commands.command(pass_context=True)
     async def stats(self, ctx, amount=1):
         """
-        Show stats about ORE
+        Show stats about NORT
         """
         channel_name = ctx.message.channel.name
         allowed_channels = parsing.parse_json('config.json')['command_channels'][ctx.command.name]
@@ -22,25 +22,18 @@ class Stats:
         headers={"user-agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.78 Safari/537.36"}
         try:
             async with ClientSession() as session:
-                async with session.get("https://api.coinmarketcap.com/v1/ticker/galactrum/", headers=headers) as response:
+                async with session.get("https://coinlib.io/api/v1/coin?key=593ce9b1bef849c6&pref=BTC&symbol=NORT", headers=headers) as response:
                     responseRaw = await response.read()
                     priceData = json.loads(responseRaw)
                     for item in priceData:
                         embed= discord.Embed(colour=0x00FF00)
-                        embed.set_author(name='ORE Information', icon_url="https://explorer.galactrum.org/images/logo.png")
-                        embed.add_field(name="Price (USD)", value="${}".format(item['price_usd']))
-                        embed.add_field(name="Price (BTC)", value="{} BTC".format(item['price_btc']))
-                        embed.add_field(name='\u200b',value='\u200b')
-                        embed.add_field(name="Volume (USD)", value="${}".format(item['24h_volume_usd']))
-                        embed.add_field(name="Market Cap", value="${}".format(item['market_cap_usd']))
-                        embed.add_field(name='\u200b',value='\u200b')
-                        embed.add_field(name="% 1h", value="{}%".format(item['percent_change_1h']))
-                        embed.add_field(name="% 24h", value="{}%".format(item['percent_change_24h']))
-                        embed.add_field(name="% 7d", value="{}%".format(item['percent_change_7d']))
-                        embed.add_field(name="Circulating Supply", value="{} ORE".format(item['available_supply']))
-                        embed.add_field(name="Total Supply", value="{} ORE".format(item['total_supply']))
-                        embed.add_field(name="Maximum Supply", value="26,280,000 ORE")
-                        embed.set_footer(text="https://coinmarketcap.com/currencies/galactrum/", icon_url="https://explorer.galactrum.org/images/logo.png")
+                        embed.set_author(name='NORT Information', icon_url="http://explorer.nort.network/images/logo.png")
+                        embed.add_field(name="Price (BTC)", value="${}".format(item['price']))                    
+                        embed.add_field(name="Volume (BTC)", value="${}".format(item['total_volume_24h']))
+                        embed.add_field(name="% 1h", value="{}%".format(item['delta_1h']))
+                        embed.add_field(name="% 24h", value="{}%".format(item['delta_24h']))
+                        embed.add_field(name="% 7d", value="{}%".format(item['delta_7d']))
+                        embed.set_footer(text="https://wallet.crypto-bridge.org/market/BRIDGE.NORT_BRIDGE.BTC", icon_url="http://explorer.nort.network/images/logo.png")
                     await self.bot.say(embed=embed)
         except:
             await self.bot.say(":warning: Error fetching prices!")
